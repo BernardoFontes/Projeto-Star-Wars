4dd1a3c90000ff72f5634ec2330fcdf337a69ad4
BernardoFontesNKWEB <bernardo.miranda@nkweb.com.br>
2021-08-11 15:05:54 -0300

    projeto starwars

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Provider from './provider/Provider';
+import Table from './components/Table';
+
+function App() {
+  return (
+    <Provider>
+      <Table />
+    </Provider>
+  );
+}
+
+export default App;
